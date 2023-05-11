d559ef72a12605631f3df52bb3f8c4900b5715d8
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2023-05-11 21:31:30 +0200

    SatSolver struct def

--- src/sat_solver.rs
+++ src/sat_solver.rs
@@ -1,1 +1,9 @@
+use crate::proposition::{CNFClause, Literal, CNF};
 
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) enum SolverJudgement {
+    Satisfiable,
+    Unsatisfiable,
+}
+
+pub(crate) struct SatSolver;
