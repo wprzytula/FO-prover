5e382ef9befdf20404d73d8a97d6ea419cbf33d7
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2023-05-14 13:27:16 +0200

    Proposition convenience constructors

--- src/proposition.rs
+++ src/proposition.rs
@@ -103,6 +103,34 @@ impl UsedVars for Proposition {
     }
 }
 
+impl Proposition {
+    fn binop(phi: Proposition, psi: Proposition, op: BinLogOpKind) -> Proposition {
+        Proposition::LogOp(LogOp::Bin(BinLogOp {
+            kind: op,
+            phi: Box::new(phi),
+            psi: Box::new(psi),
+        }))
+    }
+    pub(crate) fn implies(phi: Proposition, psi: Proposition) -> Proposition {
+        Self::binop(phi, psi, BinLogOpKind::Implies)
+    }
+    pub(crate) fn iff(phi: Proposition, psi: Proposition) -> Proposition {
+        Self::binop(phi, psi, BinLogOpKind::Iff)
+    }
+    pub(crate) fn and(phi: Proposition, psi: Proposition) -> Proposition {
+        Self::binop(phi, psi, BinLogOpKind::And)
+    }
+    pub(crate) fn or(phi: Proposition, psi: Proposition) -> Proposition {
+        Self::binop(phi, psi, BinLogOpKind::Or)
+    }
+    pub(crate) fn not(phi: Proposition) -> Proposition {
+        Proposition::LogOp(LogOp::Not(Box::new(phi)))
+    }
+    pub(crate) fn var(s: impl Into<String>) -> Proposition {
+        Proposition::Var(s.into())
+    }
+}
+
 #[cfg(test)]
 pub(crate) mod tests {
     use std::cell::RefCell;
